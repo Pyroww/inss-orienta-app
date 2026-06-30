9b390acad6d5e8f56dc009ca6d410a065631104f
Pyroww <andrergameryer@gmail.com>
2026-06-30 15:32:08 -0500

    Ajustesv23organização

--- inss-orienta/src/services/supabase.js
+++ inss-orienta/src/services/supabase.js
@@ -1,8 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Você vai pegar essas duas chaves lá no painel do seu Supabase 
-// em: Project Settings -> API
-// No Vite, puxamos as variáveis usando import.meta.env
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
